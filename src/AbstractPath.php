ed0ffdde4ef491632d5b92bb9fc2bf4d444255d1
Dan <self@danpock.me>
2022-06-12 16:31:17 -0400

    declare strict and add basic returns to get proper types

--- src/AbstractPath.php
+++ src/AbstractPath.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Astrotomic\Path;
 
 abstract class AbstractPath implements PathContract
--- src/Posix/Path.php
+++ src/Posix/Path.php
@@ -1,10 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Astrotomic\Path\Posix;
 
 use Astrotomic\Path\AbstractPath;
 use Astrotomic\Path\PathObject;
-use JetBrains\PhpStorm\Pure;
 
 class Path extends AbstractPath
 {
@@ -14,51 +15,61 @@ class Path extends AbstractPath
     public static function basename(string $path, ?string $ext = null): string
     {
         // TODO: Implement basename() method.
+        return 'UNDEFINED';
     }
 
     public static function dirname(string $path): string
     {
         // TODO: Implement dirname() method.
+        return 'UNDEFINED';
     }
 
     public static function extname(string $path): string
     {
         // TODO: Implement extname() method.
+        return 'UNDEFINED';
     }
 
     public static function format(PathObject $pathObject): string
     {
         // TODO: Implement format() method.
+        return 'UNDEFINED';
     }
 
     public static function isAbsolute(string $path): bool
     {
         // TODO: Implement isAbsolute() method.
+        return false;
     }
 
     public static function join(string ...$paths): string
     {
         // TODO: Implement join() method.
+        return 'UNDEFINED';
     }
 
     public static function normalize(string $path): string
     {
         // TODO: Implement normalize() method.
+        return 'UNDEFINED';
     }
 
     public static function parse(string $path): PathObject
     {
         // TODO: Implement parse() method.
+        return new PathObject();
     }
 
     public static function relative(string $from, string $to): string
     {
         // TODO: Implement relative() method.
+        return 'UNDEFINED';
     }
 
     public static function resolves(string ...$paths): string
     {
         // TODO: Implement resolves() method.
+        return 'UNDEFINED';
     }
 
     /**
